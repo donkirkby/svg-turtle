118409b212e0cc9bf9559b32d7e0d6744529f00f
Don Kirkby <donkirkby@gmail.com>
2022-04-05 21:58:02 -0700

    Bump version number for release.

--- svg_turtle/about.py
+++ svg_turtle/about.py
@@ -2,7 +2,7 @@
 Don't put any code in here, because it must be safe to execute in setup.py. """
 
 __title__ = 'svg_turtle'  # => name in setup.py
-__version__ = '0.4.0'
+__version__ = '0.4.1'
 __author__ = "Don Kirkby"
 __author_email__ = "donkirkby@gmail.com"
 __description__ = "Use the Python turtle to write SVG files."
