bcb24eb2a84a326aa1a9b60e84eeb40a2160a2dd
Dani J <dani.corie@jozsef.org>
2024-12-29 11:30:42 +0100

    Fixed an accidental typo

--- svg_turtle/__init__.py
+++ svg_turtle/__init__.py
@@ -2,6 +2,6 @@
 from .about import __title__, __version__, __url__
 
 # Importing TurtleGraphicsError directly from turtle will fail without tkinter.
-# noinspection PyUnresolvedReferences1
+# noinspection PyUnresolvedReferences
 from .svg_turtle import SvgTurtle, TurtleGraphicsError
 from .jupyturtle import JupyTurtle
