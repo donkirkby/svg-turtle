f35c3f3a49abe278b8c73ad051aabc86091fa877
Dani J <dani.corie@jozsef.org>
2024-12-30 22:12:04 +0100

    __init__ cleanup and file rename

--- svg_turtle/__init__.py
+++ svg_turtle/__init__.py
@@ -4,4 +4,4 @@ from .about import __title__, __version__, __url__
 # Importing TurtleGraphicsError directly from turtle will fail without tkinter.
 # noinspection PyUnresolvedReferences
 from .svg_turtle import SvgTurtle, TurtleGraphicsError
-from .IPythonTurtle import IPythonTurtle
+from .ipython_turtle import IPythonTurtle
